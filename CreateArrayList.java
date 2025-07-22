1ad2675b15bfa3da96040d88c7febc05cefa65c3
Anushka Ashutosh Patil <160115339+patilanushka5507@users.noreply.github.com>
2025-07-22 04:54:18 -0700

    Add files via upload

---
+++ CreateArrayList.java
@@ -0,0 +1,13 @@
+
+import java.util.ArrayList;
+
+class CreateArrayList {
+
+    public static void main(String[] args) {
+        ArrayList<String> name = new ArrayList<>();
+        name.add("AAKASH");
+        name.add("SANKET");
+        name.add("RAHUL");
+        System.out.println(name);
+    }
+}
